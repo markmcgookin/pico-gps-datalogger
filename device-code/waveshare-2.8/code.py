16a1866a3e4291c9276709f0833f78e695029315
Mark McGookin <mark.mcgookin@gmail.com>
2023-01-12 13:27:45 +0000

    updates

--- device-code/waveshare-2.8/code.py
+++ device-code/waveshare-2.8/code.py
@@ -1,87 +1,78 @@
 import board
-import busio
-import terminalio
-import displayio
-import neopixel
-import time
 import digitalio
+import busio
+import time
+from adafruit_debouncer import Debouncer
 
-import os
-import sdcardio
-import storage
-import sys
+# New stuff for GPS module
+import adafruit_gps
 
-from adafruit_display_text import label
-from adafruit_st7789 import ST7789
-
-import directory_functions
+# Display stuff
+import waveshareDisplay
 
 # ON BOARD LED
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
-# Release any resources currently in use for the displays
-displayio.release_displays()
+position_A_input = digitalio.DigitalInOut(board.GP2)
+position_A_input.switch_to_input(pull=digitalio.Pull.UP)
+position_A = Debouncer(position_A_input);
 
-tft_dc = board.GP8
-tft_cs = board.GP9
-tft_rst = board.GP15
+position_B_input = digitalio.DigitalInOut(board.GP3)
+position_B_input.switch_to_input(pull=digitalio.Pull.UP)
+position_B = Debouncer(position_B_input);
 
-tft_bl = digitalio.DigitalInOut(board.GP13)
-tft_bl.direction = digitalio.Direction.OUTPUT
-tft_bl.value = True
+# UART GPS Stuff
+# Setup/Define a bunch of stuff.
+TX = board.GP0
+RX = board.GP1
+
+uart = busio.UART(TX, RX, baudrate=9600, timeout=30)
+gps = adafruit_gps.GPS(uart, debug=False)
+
+# Turn on the basic GGA and RMC info (what you typically want)
+gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+
+# Set update rate to once a second (1hz) which is what you typically want.
+gps.send_command(b'PMTK220,1000')
+last_print = time.monotonic()
 
 # Clock, MOSI, MISO
 spi = busio.SPI(board.GP10, board.GP11, board.GP12)
+waveshareDisplay.setupDisplay(spi)
 
-# Mount and test the SD Card
-directory_functions.mount_sd_card(spi)
-directory_functions.print_directory("/sd")
-
-print(str(spi.frequency))
-
-display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs, reset=tft_rst)
-display = ST7789(display_bus, width=320, height=240, rotation=90)
-
-print(str(spi.frequency))
-
-# Make the display context
-splash = displayio.Group()
-display.show(splash)
-
-color_bitmap = displayio.Bitmap(320, 240, 1)
-color_palette = displayio.Palette(1)
-color_palette[0] = 0x00FF00  # Bright Green
-
-bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-splash.append(bg_sprite)
-
-# Draw a smaller inner rectangle
-inner_bitmap = displayio.Bitmap(280, 200, 1)
-inner_palette = displayio.Palette(1)
-inner_palette[0] = 0xAA0088  # Purple
-inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=20, y=20)
-splash.append(inner_sprite)
-
-# Draw a label
-text_group = displayio.Group(scale=3, x=57, y=120)
-text = "Hello World!"
-text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00)
-text_group.append(text_area)  # Subgroup for text scaling
-splash.append(text_group)
-
-count = 0
-last_print = time.monotonic()
 while True:
     current = time.monotonic()
+
+    position_A.update()
+    position_B.update()
+
+    if position_A.fell:
+        print("In position A")
+    elif position_A.rose:
+        print("Left position A")
+
+    if position_B.fell:
+        print("In position B")
+    elif position_B.rose:
+        print("Left position B")
 
     if current - last_print >= 1.0:
         last_print = current
 
-        #print("Loop " + str(count))
-        count = count + 1
+        print("Update GPS")
+        gps.update()
 
+        print("Blink")
         if led.value == True:
             led.value = False
         else:
             led.value = True
+
+        if not gps.has_fix:
+            # TODO - Flash NeoPixel Red.
+            print('Waiting for fix...')
+            continue
+
+        position = "Latitude: {0:.6f} degrees. Longitude: {1:.6f} degrees".format(gps.latitude, gps.longitude)
+        print(position)
